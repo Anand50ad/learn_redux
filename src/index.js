51b91d516c49ea1879079914d90d87d18d7182c6
Anand50ad <anand50ad@gmail.com>
2021-08-12 16:37:12 +0530

    counter using redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+//for redux we need store, action and reducer
+
+
+//action
+const increment = () =>{
+  return{
+    type : "INCREMENT"
+  }
+}
+const decrement = () =>{
+  return {
+    type: "DECREMENT"
+  }
+}
+
+//reducer will tell what is supposed to happen when we perform an action
+const counter = (state=0, action) =>{
+  switch(action.type){
+    case "INCREMENT":
+      return state+1
+    
+    case "DECREMENT":
+      return state-1  
+
+  }
+}
+
+//store will store all the states
+let store = createStore(counter);
+
+//to display our counter in the console
+store.subscribe(()=> console.log(store.getState()));
+
+//dispatcher
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
+store.dispatch(decrement());
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +53,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
